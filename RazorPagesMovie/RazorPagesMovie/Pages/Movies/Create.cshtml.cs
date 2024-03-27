ae815275087d2e2a3637d1b6fff862313757b692
patzgn <103074854+patzgn@users.noreply.github.com>
2024-03-27 20:21:29 +0100

    Add a new field

--- RazorPagesMovie/RazorPagesMovie/Pages/Movies/Create.cshtml.cs
+++ RazorPagesMovie/RazorPagesMovie/Pages/Movies/Create.cshtml.cs
@@ -10,35 +10,35 @@ using RazorPagesMovie.Models;
 
 namespace RazorPagesMovie.Pages.Movies
 {
-    public class CreateModel : PageModel
-    {
-        private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
+	public class CreateModel : PageModel
+	{
+		private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
 
-        public CreateModel(RazorPagesMovie.Data.RazorPagesMovieContext context)
-        {
-            _context = context;
-        }
+		public CreateModel(RazorPagesMovie.Data.RazorPagesMovieContext context)
+		{
+			_context = context;
+		}
 
-        public IActionResult OnGet()
-        {
-            return Page();
-        }
+		public IActionResult OnGet()
+		{
+			return Page();
+		}
 
-        [BindProperty]
-        public Movie Movie { get; set; } = default!;
+		[BindProperty]
+		public Movie Movie { get; set; } = default!;
 
-        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
+		// To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+		public async Task<IActionResult> OnPostAsync()
+		{
+			if (!ModelState.IsValid)
+			{
+				return Page();
+			}
 
-            _context.Movie.Add(Movie);
-            await _context.SaveChangesAsync();
+			_context.Movie.Add(Movie);
+			await _context.SaveChangesAsync();
 
-            return RedirectToPage("./Index");
-        }
-    }
+			return RedirectToPage("./Index");
+		}
+	}
 }
